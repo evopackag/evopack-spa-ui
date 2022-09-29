ee8c63d288a2d110057441db809b1350f83deb36
lunarcroissant <76130184+lunarcroissant@users.noreply.github.com>
2022-09-29 08:00:03 +0100

    FIX: Updated build command

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "25c4b1c464ef9fc6"; }
+/******/ 		__webpack_require__.h = function() { return "a5fe0eb9edb9773f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.460c2dcfecb637a7.hot-update.js
+++ .next/static/webpack/webpack.460c2dcfecb637a7.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a09f07678889ec2c"; }
+/******/ 	__webpack_require__.h = function() { return "a5fe0eb9edb9773f"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
